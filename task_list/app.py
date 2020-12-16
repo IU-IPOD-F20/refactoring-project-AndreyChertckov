9c9059f85d775aa751bde90477c461e94d05e9ed
Andrey Chertkov <andreychertckov@yandex.ru>
2020-12-16 18:36:56 +0300

    Refactoring: make separate router class

--- task_list/app.py
+++ task_list/app.py
@@ -2,36 +2,18 @@ from typing import Dict, List, Union
 
 from task_list.console import Console
 from task_list.data_modules import ProjectSet
-from task_list.commands import Command, CommandResponse, ParseError, AddProjectCommand, AddTaskCommand, ShowByProjectCommand, CheckCommand, UnCheckCommand, HelpCommand, DeadlineCommand, DeleteCommand, ShowByDeadlineCommand, ShowByDateCommand
+from task_list.router import Router, RouteNotFound
+from task_list.commands import Command
 
-
-class RouteNotFound(Exception):
-    pass
 
 class TaskList:
     QUIT = "quit"
 
-    def __init__(self, console: Console) -> None:
+    def __init__(self, console: Console, router: Router) -> None:
         self.console = console
         self.last_id: int = 0
         self.projects: ProjectSet = ProjectSet()
-        self.routes = {
-                "add": {
-                    "project": AddProjectCommand,
-                    "task": AddTaskCommand},
-                "view": {
-                    "by": {
-                        "project": ShowByProjectCommand,
-                        "deadline": ShowByDeadlineCommand,
-                        "date": ShowByDateCommand,
-                        }
-                },
-                "check": CheckCommand,
-                "uncheck": UnCheckCommand,
-                "help": HelpCommand,
-                "deadline": DeadlineCommand,
-                "delete": DeleteCommand
-        }
+        self.router = router
 
     def run(self) -> None:
         while True:
@@ -46,42 +28,9 @@ class TaskList:
             except ParseError as exc:
                 self.console.print(f"Parsing command return: {exc}")
 
-    def router(self, inp: str) -> Command:
-
-        def recursive_go(routes: Dict[str, Union[type, dict]], inp_list: List[str]) -> Command:
-            if not len(inp_list):
-                raise RouteNotFound(f"Command {inp} not found")
-            try:
-                current_ind, *inp_list = inp_list
-                current = routes[current_ind]
-            except KeyError:
-                raise RouteNotFound(f"Command {inp} not found")
-
-            if isinstance(current, type):
-                parameters_class = current.get_parameters_class()
-                parameters = parameters_class.parse_input_to_parameters(inp_list)
-                return current(parameters)
-            else:
-                return recursive_go(current, inp_list)
-            
-        return recursive_go(self.routes, inp.split(" "))
 
     def execute(self, command: Command) -> None:
         command_response = command.execute(self.projects)
         self.projects = command_response.new_state
         if command_response.message:
             self.console.print(command_response.message)
-
-
-    def help(self) -> None:
-        self.console.print("Commands:")
-        self.console.print("  show")
-        self.console.print("  add project <project name>")
-        self.console.print("  add task <project name> <task description>")
-        self.console.print("  check <task ID>")
-        self.console.print("  uncheck <task ID>")
-        self.console.print()
-
-    def error(self, command: str) -> None:
-        self.console.print(f"I don't know what the command {command} is.")
-        self.console.print()
